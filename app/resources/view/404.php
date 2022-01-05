9cfe12c3bb2e81179588d2d83a334686af3f9726
halillusion <halillusion@gmail.com>
2022-01-05 14:05:09 +0300

    404

---
+++ app/resources/view/404.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>404</title>
+        <style>
+        body {
+          font-family: monospace, system-ui;
+          background: #151515;
+          color: #b2b2b2;
+          padding: 1rem;
+        }
+        h1 {
+            margin: 0;
+            color: #fff;
+        }
+        h2 {
+            margin: 0;
+            color: #777;
+        }
+        </style>
+    </head>
+    <body>
+        <h1>404</h1>
+        <h2>Page not found!</h2>
+    </body>
+</html>
