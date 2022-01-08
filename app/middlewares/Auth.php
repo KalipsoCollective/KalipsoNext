a107b8acfead23691a4c46991552b69350b8bfcf
Halil IO <halillusion@gmail.com>
2022-01-08 13:37:29 +0300

    middlewares

---
+++ app/middlewares/Auth.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Middlewares;
+
+/**
+ * Auth Middleware
+ * 
+ **/
+
+class Auth {
+
+
+    public function __construct()
+    {
+
+    }
+
+    public static function with($type = 'auth') {
+
+        if ($type == 'auth' AND isset($_SESSION['auth']) !== false AND $_SESSION['auth']) {
+
+            return [
+                'status'    => true,
+            ];
+
+        } elseif ($type == 'nonAuth' AND (isset($_SESSION['auth']) === false OR ! $_SESSION['auth'])) {
+
+            return [
+                'status'    => true,
+            ];
+
+        } else {
+
+            return [
+                'status'    => false,
+                'message'   => ($type == 'nonAuth' ? 'you_have_an_session' : 'you_have_not_an_session')
+            ];
+        }
+
+    }
+
+    public static function view($point) {
+
+        $return = false;
+        if (
+            isset($_SESSION['user']->role->view_points) !== false AND 
+            in_array($point, $_SESSION['user']->role->view_points) !== false
+        ) {
+            $return = true;
+        }
+        return $return;
+
+    }
+}
