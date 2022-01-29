c4a23b7e9622a514ddf3e8b6ba73b14c7e017a72
Halil IO <halillusion@gmail.com>
2022-01-30 02:50:38 +0300

    interface components

---
+++ app/resources/view/components/alert.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ *  Bootstrap Alert Component 
+ **/
+
+return [
+	'component'	=> '
+	<div class="alert [CLASS] alert-dismissible fade show" role="alert">
+		[ICON]
+        [TITLE] [MESSAGE]
+        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+    </div>',
+	'classes'	=> [
+		'default'	=> 'alert-primary',
+		'success'	=> 'alert-success',
+		'warning'	=> 'alert-warning',
+		'error'		=> 'alert-error'
+	]
+
+];
---
+++ app/resources/view/components/icons.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ *  Material Design Icons Component - This file is required!
+ **/
+
+return [
+	'success'	=> 'mdi mdi-check-circle-outline',
+	'alert'		=> 'mdi mdi-alert-circle-outline',
+	'error'		=> 'mdi mdi-close-circle-outline',
+	'info'		=> 'mdi mdi-information-outline',
+];
