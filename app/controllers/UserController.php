32122f422c855fa5fa57df062550da9c88d8b1cf
Halil IO <halillusion@gmail.com>
2022-01-30 02:51:04 +0300

    login method alert structure

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -14,6 +14,7 @@ use App\Helpers\KN;
 final class UserController {
 
     public $request = [];
+    public $response = [];
 
     public function __construct($request = []) {
 
@@ -32,13 +33,37 @@ final class UserController {
 
         if ($this->request['request_method'] == 'POST') {
 
-            $username = $this->request['parameters']['username'];
-            $password = $this->request['parameters']['username'];
+            extract(KN::input([
+                'username'  => 'nulled_text',
+                'password'  => 'nulled_text'
+            ], $this->request['parameters']));
+
+            if (! is_null($username) AND ! is_null($password)) {
+
+                $this->response['messages'][] = [
+                    'status' => 'success',
+                    'title'  => KN::lang('alert'),
+                    'message'=> KN::lang('good')
+                ];
+
+            } else {
+
+                $this->response['messages'][] = [
+                    'status' => 'error',
+                    'title'  => KN::lang('alert'),
+                    'message'=> KN::lang('form_cannot_empty')
+                ];
+
+            }
+
+            // KN::dump($username);
+            // KN::dump($password);
         }
 
         KN::layout('user/login', [
             'title'     => KN::lang('login') . ' | ' . KN::config('app.name'),
-            'request'   => $this->request
+            'request'   => $this->request,
+            'response'  => $this->response
         ]);
 
     }
