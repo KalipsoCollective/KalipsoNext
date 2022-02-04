72cc2aa504068c8a5e828f52eb1ff4614a4c2682
Halil IO <halillusion@gmail.com>
2022-02-05 02:15:46 +0300

    view 404 -> improved.

--- app/resources/view/404.php
+++ app/resources/view/404.php
@@ -1,27 +1,11 @@
-<!doctype html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>404</title>
-        <style>
-        body {
-          font-family: monospace, system-ui;
-          background: #151515;
-          color: #b2b2b2;
-          padding: 1rem;
-        }
-        h1 {
-            margin: 0;
-            color: #fff;
-        }
-        h2 {
-            margin: 0;
-            color: #777;
-        }
-        </style>
-    </head>
-    <body>
-        <h1>404</h1>
-        <h2><?php echo (isset($message) !== false ? $message : 'Page not found!'); ?></h2>
-    </body>
-</html>
+        <div class="container account-pages">
+            <div class="row justify-content-center align-items-center">
+                <div class="col-12 col-md-6 text-center">
+                    <?php echo self::alert(); ?>
+                    <a class="btn btn-primary" href="<?php echo self::base(); ?>">
+                        <i class="mdi mdi-arrow-left"></i> <?php echo self::lang('go_to_home'); ?>
+                    </a>
+                </div>
+            </div>
+        </div>
+        
