a7cb43c49f48422fcf932a1397cab35ab7b7cfd6
Halil IO <halillusion@gmail.com>
2022-02-06 08:40:36 +0300

    404 remove default home button

--- app/resources/view/404.php
+++ app/resources/view/404.php
@@ -2,9 +2,6 @@
             <div class="row justify-content-center align-items-center">
                 <div class="col-12 col-md-6 text-center">
                     <?php echo self::alert(); ?>
-                    <a class="btn btn-primary" href="<?php echo self::base(); ?>">
-                        <i class="mdi mdi-arrow-left"></i> <?php echo self::lang('go_to_home'); ?>
-                    </a>
                 </div>
             </div>
         </div>
