2765541a4c529a05ef10031392bb922245391243
Halil IO <halillusion@gmail.com>
2022-02-06 08:41:09 +0300

    alert component is remove as rename

--- app/resources/view/components/bootstrap_temp_alert.php
+++ app/resources/view/components/bootstrap_temp_alert.php
@@ -9,13 +9,15 @@ return [
 	<div class="alert [CLASS] alert-dismissible fade show" role="alert">
 		[ICON]
         [TITLE] [MESSAGE]
-        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+        [LINK]
+        [CLOSE]
     </div>',
 	'classes'	=> [
 		'default'	=> 'alert-primary',
 		'success'	=> 'alert-success',
 		'alert'		=> 'alert-warning',
 		'error'		=> 'alert-danger'
-	]
+	],
+	'close' => '<button type="button" class="btn-close" data-bs-dismiss="alert"></button>'
 
 ];
