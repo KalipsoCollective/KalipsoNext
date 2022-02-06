c026a3fcc8ebdf297b8d2051562e9c9944eda1f7
Halil IO <halillusion@gmail.com>
2022-02-06 08:41:31 +0300

    account page test

--- app/resources/view/user/account.php
+++ app/resources/view/user/account.php
@@ -1,11 +1,7 @@
 		<div class="container account-pages">
 			<div class="row justify-content-center align-items-center">
 				<div class="col-12 col-lg-3 col-md-4 col-sm-6">
-					<div class="card kn-card">
-						<div class="card-header">
-							<?php echo self::lang('account'); ?>
-						</div>
-					</div>
+					<?php echo self::alert(); ?>
 				</div>
 			</div>
 		</div>
