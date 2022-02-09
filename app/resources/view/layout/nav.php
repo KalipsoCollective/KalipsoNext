6d845f6c7c41d64f3884fab7fc95d0dfa75423d5
Halil IO <halillusion@gmail.com>
2022-02-09 05:11:45 +0300

    nav -> logo home link assigned

--- app/resources/view/layout/nav.php
+++ app/resources/view/layout/nav.php
@@ -1,6 +1,6 @@
         <nav class="navbar navbar-expand-lg navbar-dark kn-nav fixed-top">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#"><?php echo self::config('app.name'); ?></a>
+                <a class="navbar-brand" href="<?php echo self::base(); ?>"><?php echo self::config('app.name'); ?></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
