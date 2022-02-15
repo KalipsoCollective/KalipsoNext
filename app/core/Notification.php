71a316531842cb6a14ced5c7f3caddb890e16c65
halillusion <halillusion@gmail.com>
2022-02-15 17:01:34 +0300

    nothing...

--- app/core/Notification.php
+++ app/core/Notification.php
@@ -24,7 +24,7 @@ class Notification {
         $this->types = [
             'registration' => [
                 'email' => [
-                    ''
+                    'body'
                 ]
 
             ]
