27e9355f842cddd32960d66aa554f171929cd1a8
Halil IO <halillusion@gmail.com>
2022-02-26 18:01:19 +0300

    401

---
+++ app/resources/view/401.php
@@ -0,0 +1,8 @@
+        <div class="container account-pages">
+            <div class="row justify-content-center align-items-center">
+                <div class="col-12 col-md-6 text-center">
+                    <?php echo self::alert(); ?>
+                </div>
+            </div>
+        </div>
+        
