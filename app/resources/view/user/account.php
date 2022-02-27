cfc6449187337df631464b3822e2515e0258eb7f
Halil IO <halillusion@gmail.com>
2022-02-27 04:47:36 +0300

    sample file deleted.

--- app/resources/view/user/account.php
+++ app/resources/view/user/account.php
@@ -2,6 +2,7 @@
 			<div class="row justify-content-center align-items-center">
 				<div class="col-12 col-lg-3 col-md-4 col-sm-6">
 					<?php echo self::alert(); ?>
+					Account
 				</div>
 			</div>
 		</div>
