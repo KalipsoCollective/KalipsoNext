de8ffceea6b3784f7216e615b086ddd7b2926a86
halillusion <halillusion@gmail.com>
2022-04-26 07:20:47 +0300

    nothing

--- app/Resources/view/_parts/end.php
+++ app/Resources/view/_parts/end.php
@@ -1,4 +1,3 @@
-		<?php echo \KN\Helpers\Base::sessionStoredAlert(); ?>
 		<script src="<?php echo KN\Helpers\Base::assets('libs/bootstrap/bootstrap.bundle.min.js'); ?>"></script>
 		<script src="<?php echo KN\Helpers\Base::assets('libs/kalipsotable/kalipso.table.tr.js'); ?>"></script>
 		<script src="<?php echo KN\Helpers\Base::assets('libs/kalipsotable/kalipso.table.js'); ?>"></script>
