3e5c10aed3f387056fd5df1c352945eaf6fc5f64
halillusion <halillusion@gmail.com>
2022-10-27 00:46:26 +0300

    1.0.2.7

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,7 +5,7 @@
  **/
 define('KN_START', microtime(true)); // We use it for the exec. time recorded in the log.
 define('KN_ROOT',  rtrim($_SERVER["DOCUMENT_ROOT"], '/').'/');
-define('KN_VERSION', '1.0.2.6');
+define('KN_VERSION', '1.0.2.7');
 
 /**
  * Shutdown function registration
